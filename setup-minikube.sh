7769c5458421deace196748bdebd670c83d54a1f
Julian Popescu <hi@julian.dev>
2025-09-06 00:40:30 +0200

    feat: add gitea

--- setup-minikube.sh
+++ setup-minikube.sh
@@ -19,3 +19,14 @@ kubectl config use-context minikube
 kubectl create namespace kubimo
 # set kubimo as default namespace
 kubectl config set-context --current --namespace=kubimo
+# create gitea
+helm repo add gitea-charts https://dl.gitea.io/charts/
+helm upgrade -n gitea --create-namespace --install gitea gitea-charts/gitea \
+  -f docker/gitea-values.yaml
+# create an ssh key for gitea
+temp_dir=$(mktemp -d)
+ssh-keygen -t ed25519 -q -N "" -f $temp_dir/id_ed25519
+kubectl create secret generic gitea-ssh-key \
+  --from-file=id_ed25519=$temp_dir/id_ed25519 \
+  --from-file=id_ed25519.pub=$temp_dir/id_ed25519.pub
+rm -r $temp_dir
