aa06affe461ef22b82048c90c6d38ade78879c47
Julian Popescu <hi@julian.dev>
2025-10-16 11:16:14 +0200

    feat: add ability to disable crds to charts

--- api/examples/print_crds.rs
+++ api/examples/print_crds.rs
@@ -1,14 +1,30 @@
-use std::io::stdout;
-
+use clap::Parser;
 use k8s_openapi::List;
 
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(long)]
+    helm_if: Option<String>,
+    #[arg(long)]
+    compact: bool,
+}
+
 fn main() {
-    serde_json::to_writer_pretty(
-        stdout(),
-        &List {
-            items: kubimo::all_crds(),
-            ..Default::default()
-        },
-    )
-    .unwrap()
+    let args = Args::parse();
+    let list = List {
+        items: kubimo::all_crds(),
+        ..Default::default()
+    };
+    let mut json = if args.compact {
+        serde_json::to_string(&list)
+    } else {
+        serde_json::to_string_pretty(&list)
+    }
+    .unwrap();
+    if let Some(helm_if) = args.helm_if {
+        let nl = if args.compact { "" } else { "\n" };
+        json = format!("{{{{- if {helm_if} }}}}{nl}{json}{nl}{{{{- end }}}}");
+    }
+    println!("{json}");
 }
