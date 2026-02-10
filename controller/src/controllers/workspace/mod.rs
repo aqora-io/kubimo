6ad4403d39f09a6ded70eee6a78aab3e3eec6fc0
Julian Popescu <hi@julian.dev>
2026-02-10 17:28:10 +0100

    feat: add indexer pod to controller

--- controller/src/controllers/workspace/mod.rs
+++ controller/src/controllers/workspace/mod.rs
@@ -1,3 +1,5 @@
+mod apply_indexer;
+mod apply_indexer_rbac;
 mod apply_job;
 mod apply_pvc;
 mod apply_status;
@@ -5,10 +7,12 @@ mod apply_status;
 use std::sync::Arc;
 
 use futures::prelude::*;
-use kubimo::Workspace;
 use kubimo::k8s_openapi::api::batch::v1::Job;
-use kubimo::k8s_openapi::api::core::v1::PersistentVolumeClaim;
-use kubimo::kube::runtime::{Controller, controller::Action};
+use kubimo::k8s_openapi::api::core::v1::{PersistentVolumeClaim, Pod, ServiceAccount};
+use kubimo::k8s_openapi::api::rbac::v1::{Role, RoleBinding};
+use kubimo::kube::runtime::{Controller, controller::Action, reflector::ObjectRef, watcher};
+use kubimo::prelude::*;
+use kubimo::{Runner, Workspace};
 
 use crate::backoff::default_error_policy;
 use crate::context::Context;
@@ -27,6 +31,8 @@ impl Reconciler for WorkspaceReconciler {
         futures::future::try_join_all([
             self.apply_pvc(ctx, workspace).map_ok(|_| ()).boxed(),
             self.apply_job(ctx, workspace).map_ok(|_| ()).boxed(),
+            self.apply_indexer_rbac(ctx, workspace).boxed(),
+            self.apply_indexer(ctx, workspace).boxed(),
             self.apply_status(ctx, workspace).boxed(),
         ])
         .await?;
@@ -44,9 +50,23 @@ pub async fn run(
     let bmows = ctx.api_global::<Workspace>().kube().clone();
     let pvc = ctx.api_global::<PersistentVolumeClaim>().kube().clone();
     let jobs = ctx.api_global::<Job>().kube().clone();
+    let pods = ctx.api_global::<Pod>().kube().clone();
+    let service_accounts = ctx.api_global::<ServiceAccount>().kube().clone();
+    let roles = ctx.api_global::<Role>().kube().clone();
+    let role_bindings = ctx.api_global::<RoleBinding>().kube().clone();
+    let runners = ctx.api_global::<Runner>().kube().clone();
     Ok(Controller::new(bmows, Default::default())
         .owns(pvc, Default::default())
         .owns(jobs, Default::default())
+        .owns(pods, Default::default())
+        .owns(service_accounts, Default::default())
+        .owns(roles, Default::default())
+        .owns(role_bindings, Default::default())
+        .watches(runners, watcher::Config::default(), |runner| {
+            runner
+                .namespace()
+                .map(|namespace| ObjectRef::new(&runner.spec.workspace).within(&namespace))
+        })
         .graceful_shutdown_on(shutdown_signal)
         .run(
             WorkspaceReconciler.reconcile("controller").await?,
