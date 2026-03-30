c17204ad24b02b954925c1fd79d6b3dda7fe3627
Antoine Chauvin <antoine@aqora.io>
2026-03-30 17:15:40 +0200

    feat(scripts): setup csi in minikube

--- scripts/setup-minikube.sh
+++ scripts/setup-minikube.sh
@@ -3,4 +3,11 @@
 minikube start --container-runtime=containerd \
   --network=minikube \
   --docker-opt containerd=/var/run/containerd/containerd.sock \
-  --addons=ingress,gvisor,metrics-server,dashboard
+  --addons=ingress,gvisor,metrics-server,dashboard,volumesnapshots,csi-hostpath-driver
+
+# Shamelessly copy-pasted from:
+# https://minikube.sigs.k8s.io/docs/tutorials/volume_snapshots_and_csi/
+minikube addons disable storage-provisioner
+minikube addons disable default-storageclass
+kubectl patch storageclass csi-hostpath-sc -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
+kubectl patch volumesnapshotclass csi-hostpath-snapclass --type=merge -p '{"metadata":{"annotations":{"snapshot.storage.kubernetes.io/is-default-class":"true"}}}'
